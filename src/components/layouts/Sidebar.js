0fd4f6dc15614b5aa5325c8c33edbae0a24bf267
Falua Temitope <emmat0616@gmail.com>
2023-01-18 20:40:26 +0000

    sidebar in progress

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ProductLogo from '../../assets/img/project_logo.png';
 import { navMenu } from '../../data/utils';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { BiSearch } from 'react-icons/bi';
 
 const Sidebar = (props) => {
     const { toggleSide, token } = props;
     const styles = {
-        menuItems: `flex gap-3 py-3 transition text-lg px-3 mb-2 rounded-md items-center menuItem_text cursor-pointer hover:bg-gray-100 hover:text-[#fff]`,
+        menuItems: `flex justify-between gap-3 py-3 transition text-md px-3 mb-1 rounded-md items-center menuItem_text cursor-pointer hover:bg-gray-100 hover:text-[#fff]`,
     };
     const router = useNavigate();
     const location = useLocation();
@@ -16,14 +17,19 @@ const Sidebar = (props) => {
     };
 
     return (
-        <div className="sidebar sidebar__width rounded-r-lg h-screen bg-[#0C4654] px-5 py-10 transition">
-            <div onClick={() => router('/dashboard')} className="cursor-pointer">
+        <div className="sidebar sidebar__width h-screen bg-white px-5 py-10 transition">
+            <div onClick={() => router('/dashboard')} className="cursor-pointer flex items-center">
                 <img src={ProductLogo} alt="logo" />
+                <h4 className="text-2xl font-bold pl-3">Untitled UI</h4>
             </div>
-            <div className="mt-12 transition">
+            <div className="flex justify-between items-center h-12 rounded-lg border border-gray-300 transition px-3 mt-8">
+                <BiSearch className="ml-1" size="23px" />
+                <input type="text" placeholder="Search" className="h-full border-0 pl-3 focus:outline-none" />
+            </div>
+            <div className="mt-8 transition">
                 {token !== undefined && (
-                    <div className="flex flex-col w-full h-full transition gap-3">
-                        {navMenu.map((nav, index) => {
+                    <div className="w-full h-full transition gap-3">
+                        {navMenu?.main.map((nav, index) => {
                             return (
                                 <div
                                     key={index}
@@ -34,9 +40,39 @@ const Sidebar = (props) => {
                                     }}
                                     onClick={() => goToRoute(nav)}
                                 >
-                                    {/*{nav.icon}*/}
-                                    <img src={nav.icon} alt="icon" />
-                                    <span>{nav.name}</span>
+                                    <div className="flex items-center">
+                                        <img src={nav.icon} alt="icon" className="mr-3 w-[25px]" />
+                                        <span>{nav.name}</span>
+                                    </div>
+                                    {nav.hasCount && (
+                                        <span className="rounded-[16px] w-[36px] flex justify-center items-center bg-[#F2F4F7]">
+                                            10
+                                        </span>
+                                    )}
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
+            <div className="mt-8 transition">
+                {token !== undefined && (
+                    <div className="flex flex-col w-full h-full transition gap-3">
+                        {navMenu?.others.map((nav, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className={styles.menuItems}
+                                    style={{
+                                        color: nav.route === pathname && '#FFF',
+                                        background: nav.route === pathname && '#F2F8FA28',
+                                    }}
+                                    onClick={() => goToRoute(nav)}
+                                >
+                                    <div className="flex items-center">
+                                        <img src={nav.icon} alt="icon" className="mr-3 w-[25px]" />
+                                        <span>{nav.name}</span>
+                                    </div>
                                 </div>
                             );
                         })}
@@ -45,7 +81,7 @@ const Sidebar = (props) => {
             </div>
             <div className="absolute bottom-[3rem] transition">
                 <div
-                    className="flex bg-[#FFFFFF14] py-2 px-4 cursor-pointer rounded-md text-white"
+                    className="flex bg-[#FFFFFF14] py-2 px-4 cursor-pointer rounded-md text-[#344054]"
                     onClick={toggleSide}
                 >
                     {/*<img src={ToggleIcon} alt="toggle" className="pl-0" />*/}
--- src/data/utils.js
+++ src/data/utils.js
@@ -1,7 +1,23 @@
-export const navMenu = [
-    { id: 1, name: 'Dashboard', route: '/dashboard/dashboard', icon: 'DashboardNavIcon' },
-    { id: 2, name: 'Book Test(s)', route: '/dashboard/order', icon: 'OrderNavIcon' },
-    { id: 3, name: 'Results ', route: '/dashboard/result-inbox', icon: 'ResultNavIcon' },
-    { id: 4, name: 'My Patients', route: '/dashboard/my-patients', icon: 'PatientNavIcon' },
-    { id: 4, name: 'Settings', route: '/dashboard/settings', icon: 'SettingNavIcon' },
-];
+import HomeIcon from '../assets/img/icons/homeIcon.svg';
+import DashIcon from '../assets/img/icons/dashIcon.svg';
+import ProjectIcon from '../assets/img/icons/projectIcon.svg';
+import TaskIcon from '../assets/img/icons/taskIcon.svg';
+import ReportIcon from '../assets/img/icons/reportIcon.svg';
+import UserIcon from '../assets/img/icons/usersIcon.svg';
+import SupportIcon from '../assets/img/icons/supportIcon.svg';
+import SettingIcon from '../assets/img/icons/settingsIcon.svg';
+
+export const navMenu = {
+    main: [
+        { id: 1, name: 'Home', route: '/dashboard/', icon: HomeIcon, hasCount: false },
+        { id: 2, name: 'Dashboard', route: '/dashboard/', icon: DashIcon, hasCount: true },
+        { id: 3, name: 'Projects', route: '/dashboard/order', icon: ProjectIcon, hasCount: false },
+        { id: 4, name: 'Tasks ', route: '/dashboard/result-inbox', icon: TaskIcon, hasCount: false },
+        { id: 5, name: 'Reporting', route: '/dashboard/my-patients', icon: ReportIcon, hasCount: false },
+        { id: 6, name: 'Users', route: '/dashboard/users', icon: UserIcon },
+    ],
+    others: [
+        { id: 1, name: 'Support', route: '/dashboard/', icon: SupportIcon, hasCount: false },
+        { id: 2, name: 'Settings', route: '/dashboard/', icon: SettingIcon, hasCount: false },
+    ],
+};
