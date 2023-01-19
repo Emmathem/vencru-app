e1f0acf4c23ef4cd2a190c3397aa2bd17d62b78e
Falua Temitope <emmat0616@gmail.com>
2023-01-19 10:19:53 +0000

    card section done

--- src/components/dashboard/billings/BillingCardSection.js
+++ src/components/dashboard/billings/BillingCardSection.js
@@ -16,46 +16,42 @@ const BillingCardSection = () => {
                     </div>
                 </div>
                 <div className="lg:w-3/4 w-full lg:pl-20 pl-0">
-                    <div>
-                        <div className="flex flex-col mt-6">
-                            <div className="lg:w-full bg-[#F9F5FF] border border-[#D6BBFB] rounded-[8px] p-4 transition relative">
-                                <label htmlFor="visa" className="flex items-start">
-                                    <div>
-                                        <img src={VisaIcon} alt="VisaIcon" />
+                    <div className="mt-6">
+                        <div className="radiobtn">
+                            <input type="radio" name="paymentType" id="visa" value="visa" checked />
+                            <label htmlFor="visa">
+                                <div>
+                                    <img src={VisaIcon} alt="VisaIcon" />
+                                </div>
+                                <div className="pl-3 leading-7">
+                                    <h4 className="text-[#53389E] font-medium">Visa ending in 1234</h4>
+                                    <h5 className="text-[#7F56D9] font-normal text-[14px]">Expiry 06/2024 </h5>
+                                    <div className="flex items-center cursor-pointer text-[14px]">
+                                        <div className="text-[#7F56D9] pr-2">Set as default</div>{' '}
+                                        <div className="text-[#6941C6] font-medium">Edit</div>
                                     </div>
-                                    <div className="pl-3 leading-7">
-                                        <h4 className="text-[#53389E] font-medium">Visa ending in 1234</h4>
-                                        <h5 className="text-[#7F56D9] font-normal text-[14px]">Expiry 06/2024 </h5>
-                                        <div className="flex items-center cursor-pointer text-[14px]">
-                                            <div className="text-[#7F56D9] pr-2">Set as default</div>{' '}
-                                            <div className="text-[#6941C6] font-medium">Edit</div>
-                                        </div>
+                                </div>
+                            </label>
+                        </div>
+                        <div className="radiobtn">
+                            <input type="radio" name="paymentType" id="master" value="master" />
+                            <label htmlFor="master">
+                                <div>
+                                    <img src={MasterIcon} alt="MasterIcon" />
+                                </div>
+                                <div className="pl-3 leading-7">
+                                    <h4 className="text-[#53389E] font-medium">Mastercard ending in 1234</h4>
+                                    <h5 className="text-[#7F56D9] font-normal text-[14px]">Expiry 06/2024 </h5>
+                                    <div className="flex items-center cursor-pointer text-[14px]">
+                                        <div className="text-[#7F56D9] pr-2">Set as default</div>{' '}
+                                        <div className="text-[#6941C6] font-medium">Edit</div>
                                     </div>
-                                    <input type="radio" name="paymentType" id="visa" className="absolute right-5" />
-                                </label>
-                            </div>
-                            <div className="lg:w-full bg-[#FFFFFF] border border-[#EAECF0] rounded-[8px] p-4 mt-4 transition relative">
-                                <label htmlFor="master" className="flex items-start">
-                                    <div>
-                                        <img src={MasterIcon} alt="MasterIcon" />
-                                    </div>
-                                    <div className="pl-3 leading-7">
-                                        <h4 className="text-[#53389E] font-medium">Mastercard ending in 1234</h4>
-                                        <h5 className="text-[#7F56D9] font-normal text-[14px]">Expiry 06/2024 </h5>
-                                        <div className="flex items-center cursor-pointer text-[14px]">
-                                            <div className="text-[#7F56D9] pr-2">Set as default</div>{' '}
-                                            <div className="text-[#6941C6] font-medium">Edit</div>
-                                        </div>
-                                    </div>
-                                    <input type="radio" name="paymentType" id="master" className="absolute right-5" />
-                                </label>
-                            </div>
-                            <div className="flex items-center pt-4 cursor-pointer text-[#667085]">
-                                <FiPlus size="20px" />
-                                <span className="text-[#667085] font-medium text-[14px] pl-2">
-                                    Add new payment method
-                                </span>
-                            </div>
+                                </div>
+                            </label>
+                        </div>
+                        <div className="flex items-center pt-4 cursor-pointer text-[#667085]">
+                            <FiPlus size="20px" />
+                            <span className="text-[#667085] font-medium text-[14px] pl-2">Add new payment method</span>
                         </div>
                     </div>
                 </div>
